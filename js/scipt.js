4f34c52b1ca699678f4c2fd0cd0e63042d3a3b40
DESKTOP-PE33IIO\Academy <amaldonado@techtonicgroup.com>
2018-04-04 17:02:00 -0600

    I created a Question and Answer form using Pub/Sub (observer pattern) in JavaScript.

--- js/scipt.js
+++ js/scipt.js
@@ -1,1 +1,24 @@
-alert("script included");
+//construct
+var questionA = function(){};
+
+//Initializer
+questionA.prototype.init = function () {
+  this._sendBtn = document.querySelector(".send")
+  console.log(this._sendBtn);
+  this._answerBox = document.queryselector(".question=1");
+  this._showAnswer = document.queryselector(".answer-1")
+  this._setUpListeners();
+};
+//Bind Listeners
+questionA.prototype._setUpListeners = function () {
+  var _self = this;
+  this._sendBtn.addEventListener("click", function(e){
+    var your Answer = _self.answerBox.value;
+    _self._showAnswer.innerText = yourAnswer;
+  });
+};
+
+//Instance
+window.gQuestionA = new questionA();
+//Initialize what I need for this instance
+window.gQuestionA.init();

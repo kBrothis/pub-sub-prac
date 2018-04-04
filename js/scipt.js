98c909f45c4a8a066083e186833c3647a1c56a4d
Shambre <ssenawright@techtonicgroup.com>
2018-04-04 17:00:27 -0600

    Makes the following things happen: 1.) Create a button in HTML 2.) Set up a prototyped struct 3.) Set up a _bindEvents() method 4.) Set up a click event listener 5.) Fire off console.log(e)

--- js/scipt.js
+++ js/scipt.js
@@ -1,1 +1,27 @@
-alert("script included");
+// Construct
+var questionA = function(){};
+
+//Initializer
+questionA.prototype.init = function () {
+  this._sendBtn = document.querySelector(".send");
+  this._answerBox = document.querySelector(".question-1");
+  this._showAnswer = document.querySelector(".answer-1")
+  console.log(this._sendBtn);
+  this._setUpListeners();
+};
+
+//Bind Listener
+questionA.prototype._setUpListeners = function () {  //listeners go in here
+  var _self = this;
+  this._sendBtn.addEventListener("click", function(e) {
+    var yourAnswer = _self._answerBox.value;
+    console.log(_self._showAnswer.innerText = yourAnswer);
+  });
+};
+
+
+
+//Create instance
+window.gQuestionA = new questionA();
+// Initialize what I need for this instance
+window.gQuestionA.init();

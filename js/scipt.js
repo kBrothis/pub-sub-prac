56bdeb9ad76598bff5a064066e0e4dcfc8fd9b4b
DESKTOP-645VGU4\Academy <rjackson@techtonicgroup.com>
2018-04-04 17:00:04 -0600

    question answer

--- js/scipt.js
+++ js/scipt.js
@@ -1,1 +1,24 @@
-alert("script included");
+//construct
+var questionA = function(){};
+
+//initializer
+questionA.prototype.init = function () {
+  this._sendBtn = document.querySelector(".send")
+  this._answerBox = document.querySelector(".question-1");
+  this._showAnswer = document.querySelector(".answer-1")
+  this._setUpListeners();
+};
+
+//bind listener
+questionA.prototype._setUpListeners = function () {
+  var _self = this;
+  this._sendBtn.addEventListener("click", function(e){
+    var yourAnswer = _self._answerBox.value;
+  _self._showAnswer.innerText = yourAnswer;
+});
+};
+
+//instance
+window.gQuestionA = new questionA();
+//initialize what i need for this instance
+window.gQuestionA.init();

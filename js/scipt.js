59f0d170bbcc3ce3034d1b863632c8a81fbf0d6d
Sara C <saralyssacapra@gmail.com>
2018-04-04 18:59:40 -0400

    It works!

--- js/scipt.js
+++ js/scipt.js
@@ -1,1 +1,26 @@
-alert("script included");
+// Construct
+var questionA = function(){};
+
+// initializer
+questionA.prototype.init = function () {
+  this._sendBtn = document.querySelector(".send");
+  this._answerBox = document.querySelector(".question-1");
+  this._showAnswer = document.querySelector(".answer-1");
+  this._setUpListeners();
+};
+
+// Bind Listeners
+questionA.prototype._setUpListeners = function() {
+  var _self = this;
+  this._sendBtn.addEventListener("click", function(e){
+    var yourAnswer = _self._answerBox.value;
+    _self._showAnswer.innerText = yourAnswer;
+  });
+};
+
+//
+
+// Instance
+window.gQuestionA = new questionA();
+// Initialize what I need for this instance
+window.gQuestionA.init();

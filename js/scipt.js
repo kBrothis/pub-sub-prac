d092e254e96e9d35225320b9ae8d6ca1cd7fd0cb
Allyson Gaylord <agaylord@techtonicgroup.com>
2018-04-04 17:02:22 -0600

    I created a Question and Answer form and made it work. How cool!!!

--- js/scipt.js
+++ js/scipt.js
@@ -1,1 +1,20 @@
-alert("script included");
+var questionA = function(){};
+
+questionA.prototype.init = function () {
+  this._sendBtn = document.querySelector(".send");
+  this._answerBox = document.querySelector(".question-1");
+  this._showAnswer = document.querySelector(".answer-1");
+  this._setUpListeners();
+};
+
+questionA.prototype._setUpListeners = function () {
+  var _self = this;
+  this._sendBtn.addEventListener("click", function(e){
+    var yourAnswer = _self._answerBox.value;
+    _self._showAnswer.innerText = yourAnswer;
+  console.log(_self._showAnswer);
+  });
+};
+
+window.gQuestionA = new questionA();
+window.gQuestionA.init();

67d31c130120fbcbe4805224a2512c2fe1ba73ac
Shambre <ssenawright@techtonicgroup.com>
2018-04-05 10:55:41 -0600

    Sets new properties on my questionA property prototype for question 2, adds a second var to my event listener function to produce  answer to question 2 in console.

--- js/scipt.js
+++ js/scipt.js
@@ -1,11 +1,16 @@
-// Construct
+// Constructor
 var questionA = function(){};
 
 //Initializer
 questionA.prototype.init = function () {
   this._sendBtn = document.querySelector(".send");
-  this._answerBox = document.querySelector(".question-1");
-  this._showAnswer = document.querySelector(".answer-1")
+  this._answerBox1 = document.querySelector(".question-1");
+  this._showAnswer1 = document.querySelector(".answer-1")
+
+  // I'm adding this to make a prototype for Question 2 and am setting new properties on my object with the number 2
+  this._answerBox2 = document.querySelector(".question-2");
+  this._showAnswer2 = document.querySelector(".answer-2")
+
   console.log(this._sendBtn);
   this._setUpListeners();
 };
@@ -14,8 +19,10 @@ questionA.prototype.init = function () {
 questionA.prototype._setUpListeners = function () {  //listeners go in here
   var _self = this;
   this._sendBtn.addEventListener("click", function(e) {
-    var yourAnswer = _self._answerBox.value;
-    console.log(_self._showAnswer.innerText = yourAnswer);
+    var yourAnswer = _self._answerBox1.value;
+    console.log(_self._showAnswer1.innerText = yourAnswer);
+    var yourAnswer = _self._answerBox2.value;
+    console.log(_self._showAnswer2.innerText = yourAnswer);
   });
 };
 

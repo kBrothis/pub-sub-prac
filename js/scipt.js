f28e9ddd85196c794db54cf05d811d7bab48a950
Eric Dunaway <edunaway@techtonicgroup.com>
2018-04-05 10:50:24 -0600

    extended js will now work with any number of questions&answers

--- js/scipt.js
+++ js/scipt.js
@@ -1,21 +1,30 @@
-var questionA = function(){
-};
+var questionA = function() {};
 
 questionA.prototype.init = function() {
-    this._sendBtn = document.querySelector('.send');
-    console.log(this._sendBtn);
-    this._showAnswer = document.querySelector('.answer-1');
-    this._answerBox = document.querySelector('.question-1');
-    this._setupListeners();
+  this._sendBtn = document.querySelector(".send");
+  console.log(this._sendBtn);
+  this._showAnswers = document.querySelectorAll('[class^="answer-"],[class*=" answer-"]');
+  this._answerBoxes = document.querySelectorAll('[class*=" question-"],[class^="question-"]');
+  this._setupListeners();
 };
 
-questionA.prototype._setupListeners = function(){
-    var _self = this;
-    this._sendBtn.addEventListener("click", function(e){
-        console.log(e);
-        _self._showAnswer.textContent = _self._answerBox.value;
-        _self._answerBox.value = '';
-    })
+questionA.prototype._setupListeners = function() {
+  var _self = this;
+  this._sendBtn.addEventListener("click", function(e) {
+    console.group("Survey Results");
+    console.log(e);
+    for (index = 0; index <  _self._answerBoxes.length; index++) {
+      var value =  _self._answerBoxes[index].value;
+      if (value) {
+        _self._showAnswers[index].textContent = value;
+        console.log("Answer " + (index+1) + ": " + value);
+      } else {
+        _self._showAnswers[index].textContent = "";
+      }
+      _self._answerBoxes[index].value = '';
+    }
+    console.groupEnd("Survey Results");
+  });
 };
 
 window.gQuestionA = new questionA();

5f08e9848dc271009305a556d2f361efd7b84431
LAPTOP-7QBV75FG\techtonic <jwesterfield@techtonicgroup.com>
2018-04-05 09:13:13 -0600

    added in js script and index page, made minor changes. First commit, but had to redo, because setup sourcetree wrong

--- js/scipt.js
+++ js/scipt.js
@@ -1,1 +1,24 @@
-alert("script included");
+//creation of construct
+var questionA = function() {
+
+};
+
+//initialize the function
+questionAnswer.prototype.init = function () {
+    this._sendBtn = document.querySelector(".send");
+	this._answerBox = document.querySelector("question-1");
+	this._short
+    // console.log(this._sendBtn);
+    this._setUpListeners();
+};
+
+questionA.prototype._setUpListeners = function () {
+    this._setUpListeners.addEventListener("click", function(e) {
+        console.log(e);   });
+};
+
+
+//Instance
+window.gQuestionA = new questionA();
+//Initialize what I needa for this instance
+window.gQuestionA.init();

9d9d2e2919c231c9809a29830d7e727c83c8cdd9
Allyson Gaylord <agaylord@techtonicgroup.com>
2018-04-05 10:42:51 -0600

    Got the second answer to work (I need a tutorial on commit comments...)

--- js/scipt.js
+++ js/scipt.js
@@ -2,18 +2,25 @@ var questionA = function(){};
 
 questionA.prototype.init = function () {
   this._sendBtn = document.querySelector(".send");
-  this._answerBox = document.querySelector(".question-1");
-  this._showAnswer = document.querySelector(".answer-1");
+  this._answerBoxA = document.querySelector(".question-1");
+  this._showAnswerA = document.querySelector(".answer-1");
+  this._answerBoxB = document.querySelector(".question-2");
+  this._showAnswerB = document.querySelector(".answer-2")
   this._setUpListeners();
 };
 
 questionA.prototype._setUpListeners = function () {
   var _self = this;
   this._sendBtn.addEventListener("click", function(e){
-    var yourAnswer = _self._answerBox.value;
-    _self._showAnswer.innerText = yourAnswer;
-  console.log(_self._showAnswer);
+    var yourAnswer = _self._answerBoxA.value;
+    _self._showAnswerA.innerText = yourAnswer;
+    console.log(_self._showAnswerB);
+
+    var yourAnswer = _self._answerBoxB.value;
+    _self._showAnswerB.innerText = yourAnswer;
+    console.log(_self._showAnswerA);
   });
+
 };
 
 window.gQuestionA = new questionA();

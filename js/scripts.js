1d1c7dddac687868e110bff5c019f3035ef4a880
LAPTOP-7QBV75FG\techtonic <jwesterfield@techtonicgroup.com>
2018-04-05 13:31:52 -0600

    functional gui, on one click of one button able to complete multiple queryselections.

--- js/scripts.js
+++ js/scripts.js
@@ -2,63 +2,29 @@
 var questionA = function() {
 };
 
-//creation of our questionA contructor
-var questionB = function() {
-};
-
 //QUESTION, BUTTON, && ANSWER TWO
 questionA.prototype.init = function() {
-    this._sendBtn = document.querySelector('.send-one');
-    console.log(this._sendBtn);
-    this._showAnswer = document.querySelector('.answer-1');
-    this._answerBox = document.querySelector('.question-1');
+    this._sendBtn = document.querySelector(".send");
+
+    this._showAnswerOne = document.querySelector(".answer-1");
+    this._answerBoxOne = document.querySelector(".question-1");
+
+    this._showAnswerTwo = document.querySelector(".answer-2");
+    this._answerBoxTwo = document.querySelector(".question-2");
     this._setupListeners();
 };
 
-questionA.prototype._setupListeners = function(){
-    var _self = this;
-    this._sendBtn.addEventListener("click", function(e) {
-        console.log(e);
-        if(_self._answerBox.value) {
-            console.log("value exists");
-        }
-        else {
-            alert();
-            HTMLFormControlsCollection.l
-        }
-        _self._showAnswer.textContent = _self._answerBox.value;
-        _self._answerBox.value = '';
-    });
-};
-
-
-//QUESTION, BUTTON, && ANSWER TWO
-questionB.prototype.init = function() {
-    this._sendBtn = document.querySelector('.send-two');
-    if (!this._sendBtn) {
-        console.log("Our sendBtn is wired and ready to go!");
-    }
-    console.log(this._sendBtn);
-    this._showAnswer = document.querySelector('.answer-2');
-    this._answerBox = document.querySelector('.question-2');
-    this._setupListeners();
-};
-
-questionB.prototype._setupListeners = function() {
+questionA.prototype._setupListeners = function() {
     var _self = this;
     this._sendBtn.addEventListener("click", function(e){
         console.log(e);
-        if( _self._answerBox.value == null) {
-            console.log("No value in answer 3, buddy. Maybe try putting an actual answer in here. ");
-        }
-        else {
-            _self._showAnswer.textContent = _self._answerBox.value;
-            _self._answerBox.value = '';
-        };
-    });
+        _self._showAnswerOne.textContent = _self._answerBoxOne.value;
+        _self._answerBoxOne.value = '';
+
+        _self._showAnswerTwo.innerHTML = _self._answerBoxTwo.value;
+        _self._answerBoxTwo.value = '';
+    })
 };
 
-window.questionA = new questionA();
-window.questionB = new questionB();
-window.questionA.init();
-window.questionB.init();
+window.newQuestion = new questionA();
+window.newQuestion.init();

275f52766a53999fca397e09c642160338cae9bc
DESKTOP-GJEQJER\stephen duncan <sduncan@techtonicgroup.com>
2018-04-05 10:56:33 -0600

    2 questions/answers

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ questionA.prototype.init = function () {
   this._sendBtn = document.querySelector(".send");
   this._answerBox = document.querySelector(".question-1")
   this._showAnswer = document.querySelector(".answer-1");
+  this._answerBox2 = document.querySelector(".question-2")
+  this._showAnswer2 = document.querySelector(".answer-2");
   // console.log(this._sendBtn);
   this._setupListners();
 
@@ -16,6 +18,8 @@ questionA.prototype._setupListners = function () {
     // console.log(_self._answerBox.value);
     var yourAnswer = _self._answerBox.value;
     _self._showAnswer.innerText = yourAnswer;
+    var yourAnswer2 = _self._answerBox2.value;
+    _self._showAnswer2.innerText = yourAnswer2;
   });
 };
 

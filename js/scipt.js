33d445b959ddce984e6b7b76f3b1a9f54459a859
DESKTOP-645VGU4\Academy <rjackson@techtonicgroup.com>
2018-04-05 11:27:35 -0600

    Code refined to only (as far as I know) necissary code. If the original constructor had been named survey I think I would have understood how to add the second question sooner.

--- js/scipt.js
+++ js/scipt.js
@@ -1,18 +1,14 @@
 //construct
 var questionA = function(){};
-var questionB = function(){};
+
 
 //initializer
 questionA.prototype.init = function () {
   this._sendBtn = document.querySelector(".send");
-  this._answerBox = document.querySelector(".question-1");
-  this._showAnswer = document.querySelector(".answer-1");
-  this._setUpListeners();
-};
-questionB.prototype.init = function () {
-  this._sendBtn = document.querySelector(".send");
-  this._answerBox = document.querySelector(".question-2");
-  this._showAnswer = document.querySelector(".answer-2");
+  this._answerBox1 = document.querySelector(".question-1");
+  this._showAnswer1 = document.querySelector(".answer-1");
+  this._answerBox2 = document.querySelector(".question-2");
+  this._showAnswer2 = document.querySelector(".answer-2");
   this._setUpListeners();
 };
 
@@ -21,26 +17,16 @@ questionB.prototype.init = function () {
 questionA.prototype._setUpListeners = function () {
   var _self = this;
   this._sendBtn.addEventListener("click", function(e){
-    var yourAnswer = _self._answerBox.value;
-  _self._showAnswer.innerText = yourAnswer;
+  var yourAnswer1 = _self._answerBox1.value;
+  _self._showAnswer1.innerText = yourAnswer1;
+  var yourAnswer2 = _self._answerBox2.value;
+  _self._showAnswer2.innerText = yourAnswer2;
 });
 };
 
-questionB.prototype._setUpListeners = function () {
-  var _self = this;
-  this._sendBtn.addEventListener("click", function(e){
-    var yourAnswer = _self._answerBox.value;
-  _self._showAnswer.innerText = yourAnswer;
-});
-};
 
 
 //instance
 window.gQuestionA = new questionA();
 //initialize what i need for this instance
 window.gQuestionA.init();
-
-//instance
-window.gQuestionB = new questionB();
-//initialize what i need for this instance
-window.gQuestionB.init();

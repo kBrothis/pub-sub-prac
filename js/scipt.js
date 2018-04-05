8f4105a1a1bc4763083f4ccb543569ab574b0dd1
DESKTOP-645VGU4\Academy <rjackson@techtonicgroup.com>
2018-04-05 10:36:04 -0600

    Both question answers populate!

--- js/scipt.js
+++ js/scipt.js
@@ -1,13 +1,21 @@
 //construct
 var questionA = function(){};
+var questionB = function(){};
 
 //initializer
 questionA.prototype.init = function () {
-  this._sendBtn = document.querySelector(".send")
+  this._sendBtn = document.querySelector(".send");
   this._answerBox = document.querySelector(".question-1");
-  this._showAnswer = document.querySelector(".answer-1")
+  this._showAnswer = document.querySelector(".answer-1");
   this._setUpListeners();
 };
+questionB.prototype.init = function () {
+  this._sendBtn = document.querySelector(".send");
+  this._answerBox = document.querySelector(".question-2");
+  this._showAnswer = document.querySelector(".answer-2");
+  this._setUpListeners();
+};
+
 
 //bind listener
 questionA.prototype._setUpListeners = function () {
@@ -18,7 +26,21 @@ questionA.prototype._setUpListeners = function () {
 });
 };
 
+questionB.prototype._setUpListeners = function () {
+  var _self = this;
+  this._sendBtn.addEventListener("click", function(e){
+    var yourAnswer = _self._answerBox.value;
+  _self._showAnswer.innerText = yourAnswer;
+});
+};
+
+
 //instance
 window.gQuestionA = new questionA();
 //initialize what i need for this instance
 window.gQuestionA.init();
+
+//instance
+window.gQuestionB = new questionB();
+//initialize what i need for this instance
+window.gQuestionB.init();
